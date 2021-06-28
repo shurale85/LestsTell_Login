1740c40e584555f31590d01476f880fae5d0dd69
Radik Nuriev <shurale85@gmail.com>
2021-06-28 15:39:19 +0300

    Initial Commit

---
+++ LestsTell_Login/LestsTell_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LestsTell_LoginApp.swift
+//  LestsTell_Login
+//
+//  Created by NewUSER on 28.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct LestsTell_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
